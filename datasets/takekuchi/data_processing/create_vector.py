39d63eee5fa48596cf0e01adfc1a3c002c5024a4
Wubowen <go_sann@hotmail.com>
2021-08-11 15:36:41 +0900

    cl 0 & 1

--- datasets/takekuchi/data_processing/create_vector.py
+++ datasets/takekuchi/data_processing/create_vector.py
@@ -78,7 +78,7 @@ def parse_arg():
                         help="Specify dataset dir (containing gg-train, gg-dev, gg-test)")
     # parser.add_argument('--dataset-dir', default='./data/takekuchi/source',
     #                     help="Specify dataset dir (containing gg-train, gg-dev, gg-test)")
-    parser.add_argument('--data-dir', default='./data/takekuchi/processed',
+    parser.add_argument('--data-dir', default='./data/takekuchi/processed/hip',
                         help="Specify processed data save dir")
     return parser.parse_args()
 
